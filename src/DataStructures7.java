c51d87ca29b00575bf937815e3e5ac8a4328a1a5
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-08-26 08:12:24 -0600

    created a first pass of an anagram detector algorithm that assumes that no number characters are given

---
+++ src/DataStructures7.java
@@ -0,0 +1,53 @@
+
+public class DataStructures7 {// an initial attempt at an anagram detector algorithm
+
+	public static void main(String[] args) {
+		String one = "oso";
+		String two = "oos";
+		String three = "gabriel";
+		String four = "leirbag";
+		String five = "yyy";
+		String six = "yy";
+		String seven = "sisisi";
+		String eight = "isisis";
+		String nine = "The Best Things in Life are Free";
+		String ten = "Nail biting refreshes the feet";
+		String eleven = "Nail-biting refreshes the feet";
+		System.out.println(isAnagram(one,two));
+		System.out.println(isAnagram(three,four));
+		System.out.println(isAnagram(five,six));
+		System.out.println(isAnagram(seven,eight));
+		System.out.println(isAnagram(five,one));
+		System.out.println(isAnagram(six,eight));
+		System.out.println(isAnagram(two,eight));
+		System.out.println(isAnagram(nine,ten));
+		System.out.println(isAnagram(nine,eleven));
+	}
+	
+	public static boolean isAnagram (String a, String b) {
+		char[] aSplit = a.replaceAll("\\s","").toLowerCase().toCharArray();
+		char[] bSplit = b.replaceAll("\\s","").toLowerCase().toCharArray();// converts strings to character arrays
+		if (aSplit.length != bSplit.length) {// if length not equal, immediately false
+			return false;
+		} else {
+			// each character from first word is compared to the second, and if an equal character is found, the second word's corresponding letter is turned into a number and a boolean is activated
+			for (int i = 0; i < aSplit.length; i++) {
+				boolean foundMatchingLetter = false;
+				for (int j = 0; j < bSplit.length; j++) {
+					if (aSplit[i] == bSplit[j]) {
+						if (foundMatchingLetter == false) {
+							foundMatchingLetter = true;
+							bSplit[j] = "0".toCharArray()[0];
+							break;
+						}
+					}
+				}
+				if (foundMatchingLetter != true) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
